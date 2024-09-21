2b134e6af9ada4492395fde561e8e45f2b500f8f
andre <andremorais0000@gmail.com>
2024-09-20 22:28:59 -0300

    this

---
+++ this.js
@@ -0,0 +1,13 @@
+/*
+    No JavaScript o this faz referência:
+
+    Node => module.exports
+    Web => window
+
+    Escopo:
+        => Global > Quando começamos a escrever nossa aplicação.
+        No cotexto Global, o this faz referência ao objeto global, que é o window no navegador ou ao objeto global no Node.js
+
+        => Local > Dentro de objetos ou funções se referindo ao objeto em especifico ou função que foi chamada.
+
+*/
