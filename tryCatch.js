0aafe6cbc792e36c61b29307299cd6e12616ba40
andre <andremorais0000@gmail.com>
2024-09-21 21:39:16 -0300

    added try catch and finally

---
+++ tryCatch.js
@@ -0,0 +1,17 @@
+/*
+    TRATAMENTO DE ERROS ( TRY CATCH e FINALLY)
+    Importante para ter um código mais resiliente
+
+    try-> Envolver um trecho de código que pode ocorrer um erro, o controle é tranferido para o bloco catch mais próximo.
+    catch-> Especifica uma resposta ou roda outro código caso o try não tenha rodado
+    finally-> Opcional, caso queira executar um código após rodar o try ou catch 
+
+    try{
+        todo código da aplicação
+    } catch(parametro de erro){
+        retornar erro ou status de erro 
+    } finally {
+        Opcional, caso queira executar um código após rodar o try ou catch 
+    }
+*/
+
