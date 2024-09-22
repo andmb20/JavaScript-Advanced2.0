3eca381b0f43e7cdc3b9588618aed4b41e305c80
andre <andremorais0000@gmail.com>
2024-09-21 22:04:49 -0300

    promises async await

---
+++ promises.js
@@ -0,0 +1,4 @@
+/*
+    PROMISES - ASYNC AWAIT
+        async e await são extensões das promises, introduzidas para simplificar a escrita de código assíncrono. Uma função marcada como async retorna uma promise, e a palavra-chave await é usada para esperar de forma síncrona o resultado de uma Promise. Tornando o código mais legivel e fácil de manter.
+*/
